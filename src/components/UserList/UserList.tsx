22bb12a731809a24a22d709908b6aa0c3f7e0420
maasa <maasa.giss2@gmail.com>
2020-12-24 17:49:39 +0900

    add styled-component

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+// Data Source
 import { users } from '../../data/users'
-// import { getUsers } from '../../data/getUsers'
+import { getUsers } from '../../data/getUsers'
+
+// Use CSS Modules
+import styles from './UserList.module.css'
+// Use Styled-Components
+import styled from 'styled-components'
 
 const UserList = () => {
   return null
