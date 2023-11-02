94ed6a1f656ee50b0e5cc033c5dfa040c5c8bc46
nokki-y <unoki.yoshihide@lmi.ne.jp>
2023-11-02 19:09:37 +0900

    Fixed "ListItem" component props.

--- src/components/elements/List/ListItem.tsx
+++ src/components/elements/List/ListItem.tsx
@@ -3,15 +3,15 @@ import styles from './List.module.scss'
 
 export function ListItem<T>({
   children,
-  isSelected,
+  isSelected = false,
   itemId,
   onClickItem,
 }: PropsWithChildren<{
-  itemId: T
-  isSelected: boolean
+  itemId?: T
+  isSelected?: boolean
   onClickItem?: (itemId: T) => void
 }>) {
-  const isClickable = !!onClickItem
+  const isClickable = !!itemId && !!onClickItem
   const listItemClasses = `${styles.list_item} ${isClickable ? styles.clickable : ''} ${
     isSelected ? styles.selected : ''
   }`
